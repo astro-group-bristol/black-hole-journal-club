c81774c28e0ac0f0a2a78ab052547d94439ac8c8
fjebaker <fergusbkr@gmail.com>
2024-09-10 14:22:51 +0100

    rota: allow exclusions

--- scripts/rota.py
+++ scripts/rota.py
@@ -8,18 +8,23 @@ def pop_random(items: list[str]) -> tuple[str, list[str]]:
     item = items.pop(random.randrange(len(items)))
     return (item, items)
 
-def create_ordering(members: list[str]):
+
+def create_ordering(members: list[str], exclude=None):
     m = members.copy()
     order = []
 
     while len(m) > 0:
         p, m = pop_random(m)
-        order.append(p)
+        if exclude and p not in exclude:
+            order.append(p)
 
     return itertools.cycle(order)
 
-def assemble_rota(members: list[str], starting: datetime.date, weeks_ahead: int) -> dict[datetime.date, str]:
-    ordering = create_ordering(members)
+
+def assemble_rota(
+    members: list[str], starting: datetime.date, weeks_ahead: int, exclude=None
+) -> dict[datetime.date, str]:
+    ordering = create_ordering(members, exclude=exclude)
 
     rota = []
     date = starting
@@ -30,11 +35,13 @@ def assemble_rota(members: list[str], starting: datetime.date, weeks_ahead: int)
 
     return rota
 
+
 def format_rota(rota: dict[datetime.date, str]) -> str:
     data = []
-    for (date, person) in rota:
+    for date, person in rota:
         data.append([date.isoformat(), person])
     return tabulate.tabulate(data, headers=["Date", "Presenter"], tablefmt="github")
+
 
 if __name__ == "__main__":
     members = [
@@ -49,11 +56,17 @@ if __name__ == "__main__":
         "Fergus",
         "Andy",
     ]
+    exclude_from_rota = [
+        "Anantanarayanan",
+    ]
     # needed a random seed so that either andy, tom or fergus was the first to
     # present
     random.seed(42 + 11)
-    rota = assemble_rota(members, datetime.date(2024, 9, 17), 10)
+    rota = assemble_rota(
+        members,
+        datetime.date(2024, 9, 17),
+        len(members) - len(exclude_from_rota),
+        exclude=exclude_from_rota,
+    )
 
     print(format_rota(rota))
-
-
